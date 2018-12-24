321757e5884456dbfc1f4400744a9a62d09c5ba0
Juan Carlos Pulido <juancarlospulido97@gmail.com>
2018-12-24 14:15:37 +0000

    Check de la variable DATABASE_URL

--- src/db.py
+++ src/db.py
@@ -1,5 +1,6 @@
 import urllib
 import flask
+import sys
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -7,6 +8,11 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask('__main__')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+if os.environ.get('DATABASE_URL'):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+else:
+    print("ERROR: DATABASE_URL not set", file=sys.stderr)
+    sys.exit()
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
